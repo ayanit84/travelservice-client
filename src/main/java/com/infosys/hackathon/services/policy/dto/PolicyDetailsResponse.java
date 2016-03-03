0008f0632bf44ce8e4c492c2772a05a07bcf3953
gurupadhy <gd87200@gmail.com>
2016-03-03 12:29:10 +0530

    policy response changed

--- src/main/java/com/infosys/hackathon/services/policy/dto/PolicyDetailsResponse.java
+++ src/main/java/com/infosys/hackathon/services/policy/dto/PolicyDetailsResponse.java
@@ -1,24 +1,18 @@
 package com.infosys.hackathon.services.policy.dto;
 
-import java.util.List;
-
 import com.infosys.hackathon.services.ServiceResponse;
 import com.infosys.hackathon.services.policy.EligibilityInformation;
 
 public class PolicyDetailsResponse extends ServiceResponse {
 	
-	private List<EligibilityInformation> eligibilityDetails;
+	private EligibilityInformation eligibilityDetails;
 
-	public List<EligibilityInformation> getEligibilityDetails() {
+	public EligibilityInformation getEligibilityDetails() {
 		return eligibilityDetails;
 	}
 
-	public void setEligibilityDetails(
-			List<EligibilityInformation> eligibilityDetails) {
+	public void setEligibilityDetails(EligibilityInformation eligibilityDetails) {
 		this.eligibilityDetails = eligibilityDetails;
 	}
-	
-	
-	
 
 }
