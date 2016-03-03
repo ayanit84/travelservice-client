643799e0eeaa2ebf3ff1aed040e66ce4707e51ea
saikamini <saikamini.9@gmail.com>
2016-03-03 16:44:38 +0530

    added support of path variable in get reqest, updated serach response of contanct service.

--- src/main/java/com/infosys/hackathon/services/client/ServiceClient.java
+++ src/main/java/com/infosys/hackathon/services/client/ServiceClient.java
@@ -7,7 +7,7 @@ import com.infosys.hackathon.services.exceptions.TravelServiceException;
 public interface ServiceClient<T extends ServiceRequest, V extends ServiceResponse> {
 	public String getServiceName();
 
-	public V getRequest(String handlerName, Class<V> responseType) throws TravelServiceException;
+	public V getRequest(String handlerName, Class<V> responseType, String... urlParams) throws TravelServiceException;
 
 	public V postRequest(String handlerName, T request, Class<V> responseType)
 			throws TravelServiceException;
--- src/main/java/com/infosys/hackathon/services/contact/SearchResponse.java
+++ src/main/java/com/infosys/hackathon/services/contact/SearchResponse.java
@@ -1,12 +1,33 @@
 package com.infosys.hackathon.services.contact;
 
+import java.util.List;
+
 import com.infosys.hackathon.services.ServiceResponse;
 import com.infosys.hackathon.services.contact.ContactInformation;
+import com.infosys.hackathon.services.directory.EmployeeDirectoryInformation;
 
 public class SearchResponse extends ServiceResponse {
 	private ContactInformation country;
-
+	private EmployeeDirectoryInformation bphr;
+	private EmployeeDirectoryInformation countryHead;
+	
 	public SearchResponse() {
+	}
+	
+	public EmployeeDirectoryInformation getBphr() {
+		return bphr;
+	}
+
+	public void setBphr(EmployeeDirectoryInformation bphr) {
+		this.bphr = bphr;
+	}
+
+	public EmployeeDirectoryInformation getCountryHead() {
+		return countryHead;
+	}
+
+	public void setCountryHead(EmployeeDirectoryInformation countryHead) {
+		this.countryHead = countryHead;
 	}
 
 	public ContactInformation getCountry() {
@@ -16,4 +37,6 @@ public class SearchResponse extends ServiceResponse {
 	public void setCountry(ContactInformation country) {
 		this.country = country;
 	}
+	
+	
 }
